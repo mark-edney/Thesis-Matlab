dc41245418f2291da7cefa73a2cebab4f322a097
Mark Edney <m2edney@gmail.com>
2021-01-28 21:23:13 -0500

    writing article update

--- Untitled.m
+++ Untitled.m
@@ -1,16 +1,2 @@
-close all
 
-figure;
-hold on;
-D=0.1;
-tau=1;
-Dis=1./sqrt(4*pi.*D).*exp(-(1-x./tau).^2./(4.*x.*D));
-y=cumtrapz(Dis(x));
-plot(x,y,'LineWidth',2.5);
-plot(x,cstr(x),'LineWidth',2.5);
-title('Segregation Quantity (S)','FontSize',20);
-xlabel('Dimensionless time($\theta = \frac{time}{\bar{t}_{avg}})$','Interpreter','Latex');
-ylabel('$F(\theta )$','Interpreter','Latex');
-xline(1,'LineWidth',2.5);
-yline(1,'LineWidth',2.5);
-legend('Reactor','CSTR','Segregation (S)','Interpreter','Latex')
+Table[:,1:2]=DoMeterslogfile[:,1:2]
